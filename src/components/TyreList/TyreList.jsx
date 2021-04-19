5d602818e09cea753750b82f6253cf171e19c4f0
gel00 <gellaigabor@gmail.com>
2021-04-19 09:06:47 +0100

    no result added

--- src/components/TyreList/TyreList.jsx
+++ src/components/TyreList/TyreList.jsx
@@ -3,13 +3,17 @@ import styles from "./TyreList.module.scss";
 import TyreCard from "../TyreCard";
 
 const TyreList = ({ tyreList }) => {
-  return (
-    <div className={styles["tyre-list"]}>
-      {tyreList.map((tyre) => (
-        <TyreCard key={tyre["_id"]} tyre={tyre} />
-      ))}
-    </div>
-  );
+  if (tyreList.length) {
+    return (
+      <div className={styles["tyre-list"]}>
+        {tyreList.map((tyre) => (
+          <TyreCard key={tyre["_id"]} tyre={tyre} />
+        ))}
+      </div>
+    );
+  } else {
+    return <p>No Result</p>;
+  }
 };
 
 export default TyreList;
